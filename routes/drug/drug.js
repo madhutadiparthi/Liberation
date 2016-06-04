d31910f27ebe93d14a38b797b4847165e6eaa03e
Ashish Mathur <reachashishmathur@gmail.com>
2016-06-04 07:09:50 +0530

    Modified createDrug service and used it to fill up the database from the Utils project

--- routes/drug/drug.js
+++ routes/drug/drug.js
@@ -11,22 +11,45 @@ exports.create = function(req, res) {
 
 //POST new user creation form
 exports.doCreate = function (req, res) {
-	drug.create({
-		name: req.body.Name,
-		companyName: req.body.CompanyName,
-		strength: req.body.Strength,
-		modifiedOn: Date.now()
-	}, function(err, user) {
-		if(err) {
-			console.log(err);
-			if(err.code===11000) {
-				res.redirect('/drug/new?exists=true');
-			} else {
+	var data = req.body;
+	console.log(data);
+	
+	function addDrug(callback, data) {
+		var obj = JSON.parse(Object.keys(data));
+		callback(obj);
+	}
+	
+	function createDrug(data, json) {
+		console.log("name: " + data.name);
+		drug.create({
+			name: data.name,
+			strength: data.strength,
+			type: data.type,
+			companyName: data.company,
+			genericName: data.genericName,
+			modifiedOn: Date.now()
+		}, function(err, drugData) {
+			if(err) {
+				console.log(err);
 				res.redirect('/?error=true');
 			}
-		}else {
-			//SUCCESS
-			console.log("Drug added successfully: " + user);
+			else {
+				//SUCCESS
+				if (json) {
+					res.writeHead(200, {'Content-Type': 'application/json'});
+					res.write("Drug created");
+					res.end();
+				}
+				else {
+					res.writeHead(200, {'Content-Type' : 'text/html'});
+					res.write('<html><head/><body>');
+					res.write('Drug Created');
+					res.end('</body>');
+				}
+				console.log("Drug created and saved: " + drugData);
+			}
+		});
 		}
-	});
+		
+		addDrug(createDrug, data, req.accepts('json'));
 };
